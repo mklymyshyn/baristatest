7216a46afe31775da484587045108d57a1ace191
Denis Plisko <denpliss@gmail.com>
2011-06-25 13:37:56 +0300

    Added landing page

--- baristatest/settings.py
+++ baristatest/settings.py
@@ -72,9 +72,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'baristatest.urls'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+        PROJROOT('baristatest/templates'),
 )
 
 INSTALLED_APPS = (
